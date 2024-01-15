05b53d882622ef1fd5275289617149d7ec8b46b7
hawazaft <156694389+hawazaft@users.noreply.github.com>
2024-01-16 01:26:01 +0800

    Add files via upload

---
+++ shop.js
@@ -0,0 +1,29 @@
+//for selected option 
+var selected1 = document.querySelector(".select1");
+selected1.addEventListener("change", () => {
+
+    let select = document.querySelector(".select1").value;
+    if (select != "") {
+        window.location.href = `./${select}.html`;
+    }
+});
+var selected2 = document.querySelector(".select2");
+selected2.addEventListener("change", () => {
+
+    let select = document.querySelector(".select2").value;
+    if (select != "") {
+        window.location.href = `./${select}.html`;
+    }
+});
+var selected2 = document.querySelector(".select3");
+selected2.addEventListener("change", () => {
+
+    let select = document.querySelector(".select3").value;
+    if (select != "") {
+        window.location.href = `./${select}.html`;
+    }
+});
+// selectedOpt()
+function storeData(productDetails) {
+    sessionStorage.setItem("selectedProduct", JSON.stringify(productDetails));
+}
